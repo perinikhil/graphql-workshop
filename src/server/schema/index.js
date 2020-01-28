8a98db9a15bd4f0a1e2f9890c7c6ff9928fae6fe
Lakshya Ranganath <lakshya.ranganath@booking.com>
2020-01-28 15:48:53 +0100

    add schema object

--- src/server/schema/index.js
+++ src/server/schema/index.js
@@ -1,33 +1,32 @@
 const { gql } = require('apollo-server');
 
-const typeDefs = [
-  gql`
-    type Query {
-      hotels: [Hotel!]!
+const schema = {
+  typeDefs: [
+    gql`
+      type Query {
+        hotels: [Hotel!]!
+      }
+    `,
+    gql`
+      type Hotel {
+        id: ID!,
+        name: String!
+      }
+    `,
+  ],
+  resolvers: {
+    Query: {
+      hotels: () => ([{
+        id: 1,
+        name: 'Elmas'
+      }])
+    },
+    Hotel: {
+      id: (hotel) => hotel.id,
+      name: (hotel) => hotel.name,
     }
-  `,
-  gql`
-    type Hotel {
-      id: ID!,
-      name: String!
-    }
-  `,
-]
-
-const resolvers = {
-  Query: {
-    hotels: () => ([{
-      id: 1,
-      name: 'Elmas'
-    }])
-  },
-  Hotel: {
-    id: (hotel) => hotel.id,
-    name: (hotel) => hotel.name,
   }
-}
+};
 
-module.exports = {
-  typeDefs,
-  resolvers,
-}
+
+module.exports = schema;
