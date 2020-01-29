854919727bf1b6a19f05cfabf57d257a2a828073
Peri Nikhil <peri.nikhil@booking.com>
2020-01-29 12:21:31 +0100

    feat(ApolloClient): connect App with ApolloClient

--- src/client/App.js
+++ src/client/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import SearchResultsList from './SearchResults/SearchResultsList';
+import { ApolloProvider } from '@apollo/react-hooks';
+import client from './client';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/search">
-          <SearchResultsList />
-        </Route>
-        <Redirect exact from="/" to="/search" />
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route exact path="/search">
+            <SearchResultsList />
+          </Route>
+          <Redirect exact from="/" to="/search" />
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- src/client/SearchResults/SearchResultsList.jsx
+++ src/client/SearchResults/SearchResultsList.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const HOTELS_QUERY = gql`
+  query hotelsQuery {
+    hotels {
+      id
+      name
+    }
+  }
+`;
 
 const SearchResultsList = () => {
+  const { loading, error, data } = useQuery(HOTELS_QUERY);
+  
+  if (loading) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
+  if (error) {
+    throw(error);
+  }
+
+  console.log('data: ', data);
   return (
     <div>
-      Search Results List
+      {data.hotels.map((hotel) => (
+        <div>
+          Name: {hotel.name}
+        </div>
+      ))}
     </div>
   )
 }
