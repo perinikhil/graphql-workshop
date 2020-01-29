1fc927051233dc6f6b5e2a35de726fb510889d35
Peri Nikhil <peri.nikhil@booking.com>
2020-01-29 15:47:15 +0100

    fix(graphql-server): add review score to Hotel

--- src/server/schema/index.js
+++ src/server/schema/index.js
@@ -1,5 +1,7 @@
 const { gql } = require("apollo-server");
 
+const API_URL = 'http://localhost:5000/api';
+
 const schema = {
   typeDefs: [
     gql`
@@ -15,29 +17,31 @@ const schema = {
     `,
     gql`
       type Hotel {
-        id: ID!
-        name: String!
+        id: ID!,
         city: String!
         imageUrl: String!
+        name: String!
         price: Price!
+        reviewScore: Float!
       }
     `
   ],
   resolvers: {
     Query: {
-      hotels: () =>
-        fetch("http://localhost:5000/api/hotels/").then(res => res.json())
+      hotels: () => fetch(`${API_URL}/hotels/`)
+        .then((res) => res.json())
     },
     Price: {
-      amount: price => price.price,
-      currencyCode: price => console.log(price) || price.currencyCode
+      amount: (price) => price.price,
+      currencyCode: (price) => price.currencyCode,
     },
     Hotel: {
-      id: hotel => hotel.id,
-      name: hotel => hotel.name,
-      city: hotel => hotel.city,
-      imageUrl: hotel => hotel.imageUrl,
-      price: hotel => hotel.priceInfo
+      id: (hotel) => hotel.id,
+      city: (hotel) => hotel.city,
+      imageUrl: (hotel) => hotel.imageUrl,
+      name: (hotel) => hotel.name,
+      price: (hotel) => hotel.priceInfo,
+      reviewScore: (hotel) => hotel.score,
     }
   }
 };
