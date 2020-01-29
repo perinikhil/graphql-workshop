94c9b3d7e74033dfc8ccd68677bf68db6234322f
Peri Nikhil <peri.nikhil@booking.com>
2020-01-29 11:12:19 +0100

    feat(SearchResults): initial commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/search">
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              {/* <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p> */}
+              {/* <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a> */}
+            </header>
+          </div>
+        </Route>
+        <Redirect exact from="/" to="/search" />
+      </Switch>
+    </Router>
   );
 }
 
