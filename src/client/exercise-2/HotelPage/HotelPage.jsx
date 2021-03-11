d9a438b890450df70df0667843de29219f703bde
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2021-03-11 13:18:13 +0100

    feat: add client/exercise-2

---
+++ src/client/exercise-2/HotelPage/HotelPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HotelPage.css";
+
+const HotelPage = () => {
+  return (
+    <div className="hotel-page">
+      <p>We'll be using this as part of exercise-3</p>
+    </div>
+  );
+}
+
+export default HotelPage;
