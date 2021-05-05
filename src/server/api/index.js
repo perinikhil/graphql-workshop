af7597774b7ec5cf547178d7aec4af88e299b261
Peri Nikhil <peri.nikhil@booking.com>
2021-05-05 15:39:56 +0200

    fix(graphql-server): fix vercel prod stuff

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -4,7 +4,9 @@ const schema = require("../schema");
 const server = new ApolloServer({
   typeDefs: schema.typeDefs,
   resolvers: schema.resolvers,
-  cors: true
+  cors: true,
+  introspection: true,
+  playground: true,
 });
 
 module.exports = server.createHandler({
